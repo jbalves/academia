3826cccaa5a82e1aa7380ae3a4978aa3bbbd4142
andersonmoliveira <anderson.moliveira23@gmail.com>
2015-02-13 17:17:39 -0400

    Update ClienteMB.java
    ptimeiro teste

--- codigofonte/academiaweb/src/br/com/academia/controller/ClienteMB.java
+++ codigofonte/academiaweb/src/br/com/academia/controller/ClienteMB.java
@@ -2,8 +2,23 @@ package br.com.academia.controller;
 
 import javax.faces.bean.ManagedBean;
 
+import br.com.academia.model.beam.Cliente;
+import br.com.academia.model.dao.ClienteDAO;
+
 @ManagedBean
 public class ClienteMB {
 
+	private Cliente cliente = new Cliente();
+	private ClienteDAO clienteDAO = new ClienteDAO();
 	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public void getSalvar(){
+		System.out.println("Nome: " +cliente.getNome());
+	}
 }
