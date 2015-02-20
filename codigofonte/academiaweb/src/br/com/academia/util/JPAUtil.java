684f27f91d9ee35e2244e0be20d3f703e5a15382
Jeferson Barros <soldier.jhonson@gmail.com>
2015-02-20 14:02:39 -0400

    Atualização das telas

--- codigofonte/academiaweb/src/br/com/academia/util/JPAUtil.java
+++ codigofonte/academiaweb/src/br/com/academia/util/JPAUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("financas");
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("academiaweb");
 
 	public EntityManager getEntityManager() {
 		return entityManagerFactory.createEntityManager();
